966178e720eb654726f63b6df2d435fa2c3b7d40
uthapa82 <tupendra022@gmail.com>
2022-07-21 07:13:33 -0400

    Sample Answer

--- nearest_power.py
+++ nearest_power.py
@@ -1,22 +1,17 @@
 # gfg fork python Nearest Problem Contest 1
 # You are given two numbers A and B. When A is raised to some power P, we get a
 # number X. What value of X that is closet to B
-import itertools
+import math
 
 def nearestPower(A, B):
-    result = 1
-    while True:
-        result *= A
-        if result > B:
-            return int(result/A)
-    # for i in range(1, B):
-    #     if pow(A, i) <= B:
-    #         result = pow(A, i)
-    #         break
-    #     else:
-    #         continue
-            
-    return int(result)
+    result = math.floor(math.log(B, A))
+    newResult = result + 1
+    result1 = A ** result
+    result2 = A ** newResult
+    if(abs(result1 - B) > abs(result2 - B)):
+        return result2
+    else:
+        return result1
     
 def main():
     A = int(input("Enter A: "))
