abe309b9cd34a952a99098cd7d87517c3da674c4
uthapa82 <tupendra022@gmail.com>
2022-08-19 21:15:46 -0400

    part 2

--- OOP concepts refresher/oop_2.py
+++ OOP concepts refresher/oop_2.py
@@ -31,11 +31,24 @@ class SoftwareEngineer:
     def __str__(self):
         info = f"name = {self.name}, age = {self.age}, level = {self.level}"
         return info
+    
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+    
+    # using decorators 
+    @staticmethod
+    def entry_salary(age):
+        if age < 25:
+            return 5000
+        if age < 30:
+            return 7000
+        return 9000  
 
 # instance 
 se_1 = SoftwareEngineer("Max", 20, "Junior", 5000)
 
 se_2 = SoftwareEngineer("Lisa", 25, "Senior", 7000)
+se_3 = SoftwareEngineer("Lisa", 25, "Senior", 7000)
 
 se_1.code()
 se_2.code()
@@ -51,3 +64,10 @@ print("\nPrinting information using only dunder method ")
 print(se_1)
 print(se_2)
 
+# comparing if se_2 and se_3 are same
+# if we have not used dunder method the following will compare memory location
+# not the values 
+print(se_2 == se_3)
+
+print(se_1.entry_salary(24))
+print(SoftwareEngineer.entry_salary(27))
