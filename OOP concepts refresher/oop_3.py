1067e471174f6b94eb58958e23014dccddc4ed74
uthapa82 <tupendra022@gmail.com>
2022-08-23 08:36:01 -0400

    polymorphism

--- OOP concepts refresher/oop_3.py
+++ OOP concepts refresher/oop_3.py
@@ -13,15 +13,39 @@ class SoftwareEngineer(Employee):
     def __init__(self, name, age, salary, level):
         super().__init__(name, age, salary)
         self.level = level
+    # overriding 
+    def work(self):
+        print(f"{self.name} is coding..")
+          
+    def debug(self):
+        print(f"{self.name} is debugging....")
 
 
 class Designer(Employee):
-    pass 
+    # overriding 
+    def work(self):
+        print(f"{self.name} is designing..")
+    def draw(self):
+        print(f"{self.name} is drawing.....")
 
 se = SoftwareEngineer("Abhishek", 21, 6000, "Junior")
 # print(se.name, se.age)
 se.work()
+se.debug()
 
-de = SoftwareEngineer("Roshan", 26, 8000, "Senior")
+de = Designer("Roshan", 26, 8000)
 # print(de.name, de.age)
 de.work()
+de.draw()
+
+# polymorphism 
+emplyees = [SoftwareEngineer("Abhishek", 21, 6000, "Junior"),
+            SoftwareEngineer("Roshan", 25, 10000, "Senior"),
+            Designer("Ojha", 26, 8000)]
+
+print("\n Polymorphism example.....")
+def motivate_employees(employees):
+    for employee in employees:
+        employee.work()
+        
+motivate_employees(emplyees)
