fa2632c3b1c0695ebdf0ee0a58dfebaff01a69ad
uthapa82 <tupendra022@gmail.com>
2022-08-25 07:49:27 -0400

    list continued

--- August_2022/list_gfg.py
+++ August_2022/list_gfg.py
@@ -3,9 +3,9 @@
 # O(1) --> time complexity, O(n)-----> space complexity 
 
 # example of multidimensional list 
-list = [['Ashburn', 'VA'], ['Lawrence', 'NJ']]
-print(list[1][0])
-print(list[1][1])
+list2 = [['Ashburn', 'VA'], ['Lawrence', 'NJ']]
+print(list2[1][0])
+print(list2[1][1])
 
 # complexities of accessing elements in a list :-
 # time complexity: O(1) 
@@ -20,3 +20,18 @@ user_input = n.split()
 
 
 print("The list is: ", user_input)
+
+# list comprehension syntax
+# newList = [expression(element) for element in oldList if condition]
+odd_square = [x ** 2 for x in range(1, 11) if x % 2 == 1]
+print(odd_square)
+
+# union of 2 lists 
+def union(lst1, lst2):
+    final_list = list(set(lst1) | set(lst2))
+    return final_list
+
+lst1 = [2, 4, 6, 8]
+lst2 = [1,2,3,4,5,6,7,8]
+print(union(lst1, lst2))
+
