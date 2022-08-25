05e3aeceb48343f3ca8273bfbe897774ce98c664
uthapa82 <tupendra022@gmail.com>
2022-08-24 22:09:08 -0400

    decorators 2

--- OOP concepts refresher/decorators.py
+++ OOP concepts refresher/decorators.py
@@ -1,15 +1,21 @@
 def start_end_decorator(func):
     
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print("Start")
-        func()
+        result = func(*args, **kwargs)
         print("End")
+        return result
     return wrapper
 
 @start_end_decorator
+def add5(x):
+    return x + 5
+
 def print_name():
     print("Alex")
     
 #print_name = start_end_decorator(print_name)
 
 print_name()
+result = add5(10)
+print(result)
