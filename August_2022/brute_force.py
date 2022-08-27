00ac44b298911724604984e5b8d38ccdc5fe80be
uthapa82 <tupendra022@gmail.com>
2022-08-26 22:01:13 -0400

    brute force 2

--- August_2022/brute_force.py
+++ August_2022/brute_force.py
@@ -10,10 +10,22 @@ def repeat(x):
                 
     return repeated
 
+def counter():
+    list_2 = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+    new = []
+    for a in list_2:
+        n = list_2.count(a)
+        if n > 1:
+            if new.count(a) == 0:
+                new.append(a)
+    print(new)
+
 def main():
     list_1 = [10, 20, 30, 20, 30, 40, 50, -20, 60, -20]
     print("Brute Force Approach ")
     print(repeat(list_1))
+    
+    counter()
     
     
 if __name__ == "__main__":
