9d2cc3a2c660850fd7d4a3da4c53b3422243ca62
uthapa82 <tupendra022@gmail.com>
2022-08-26 22:14:28 -0400

    list comprehension

--- August_2022/brute_force.py
+++ August_2022/brute_force.py
@@ -20,12 +20,19 @@ def counter():
                 new.append(a)
     print(new)
 
+# print duplicate using list comprehension
+def duplicate_item():
+    lst = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+    result = list(set([x for x in lst if lst.count(x) > 1]))
+    return print(result)
+    
 def main():
     list_1 = [10, 20, 30, 20, 30, 40, 50, -20, 60, -20]
     print("Brute Force Approach ")
     print(repeat(list_1))
     
     counter()
+    duplicate_item()
     
     
 if __name__ == "__main__":
