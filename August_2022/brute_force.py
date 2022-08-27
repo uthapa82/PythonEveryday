5c0ef7c6eb79d5d7287b0819dd6c580ba2c51866
uthapa82 <tupendra022@gmail.com>
2022-08-26 22:18:07 -0400

    dictionary

--- August_2022/brute_force.py
+++ August_2022/brute_force.py
@@ -25,7 +25,24 @@ def duplicate_item():
     lst = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
     result = list(set([x for x in lst if lst.count(x) > 1]))
     return print(result)
+
+# using dictionary approach 
+def duplicate_dict():
+    lst = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+    new_dict, new_list = {}, []
     
+    for i in lst:
+        if not i in new_dict:
+            new_dict[i] = 1
+        else:
+            new_dict[i] += 1
+    
+    for key, values in new_dict.items():
+        if values > 1:
+            new_list.append(key)
+    result = print(new_list)
+    return result 
+
 def main():
     list_1 = [10, 20, 30, 20, 30, 40, 50, -20, 60, -20]
     print("Brute Force Approach ")
@@ -33,7 +50,8 @@ def main():
     
     counter()
     duplicate_item()
-    
+    print("\n Dictionary approach: ")
+    duplicate_dict()
     
 if __name__ == "__main__":
     main()
