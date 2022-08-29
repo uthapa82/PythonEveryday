2a47dbd88580cd1210f68584fb51aa8f9dff7cf1
uthapa82 <tupendra022@gmail.com>
2022-08-28 21:23:12 -0400

    P2

--- August_2022/statement_test.py
+++ August_2022/statement_test.py
@@ -1,5 +1,5 @@
 
-# prints put words that start with 's'
+# P1.prints put words that start with 's'
 def word_with_s():
     string_1 = "Print only the words that start with s in this sentence"
     words = string_1.split()
@@ -7,25 +7,29 @@ def word_with_s():
         if word[0] == 's':
             print(word)
 
-# even number from 0 to 10 
+# P2.even number from 0 to 10 
 # create a list of all numbers between 1 and 50 divisible by 3
 def list_example():
-    pass
+    list_1 = [value for value in range(1, 10) if value%2 == 0]
+    print("List ofeven numebr \n", list_1)
+    list_2 = [x for x in range(1, 50) if x%3 == 0]
+    print("List of number divisible by 3 between 1 and 50\n", list_2)
 
-# length of word even or odd 
+# P3.length of word even or odd 
 def len_of_str():
     pass
 
-# fizzbuzz for nultiple of 3 and 5 
+# P4fizzbuzz for nultiple of 3 and 5 
 def fizz_buzz():
     pass
 
-# list of first letter of every word in string 
+# P5. list of first letter of every word in string 
 def list_0f_str():
     pass
 
 def main():
     word_with_s()
+    list_example()
 
 if __name__ == "__main__":
     main()
