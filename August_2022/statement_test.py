4549e4dc13523ea1b8537271079520631a3bf449
uthapa82 <tupendra022@gmail.com>
2022-08-28 21:16:34 -0400

    P1

---
+++ August_2022/statement_test.py
@@ -0,0 +1,31 @@
+
+# prints put words that start with 's'
+def word_with_s():
+    string_1 = "Print only the words that start with s in this sentence"
+    words = string_1.split()
+    for word in words:
+        if word[0] == 's':
+            print(word)
+
+# even number from 0 to 10 
+# create a list of all numbers between 1 and 50 divisible by 3
+def list_example():
+    pass
+
+# length of word even or odd 
+def len_of_str():
+    pass
+
+# fizzbuzz for nultiple of 3 and 5 
+def fizz_buzz():
+    pass
+
+# list of first letter of every word in string 
+def list_0f_str():
+    pass
+
+def main():
+    word_with_s()
+
+if __name__ == "__main__":
+    main()
