aca7e5a600a30da6d1896b3fa5b20a5a022eb35f
uthapa82 <tupendra022@gmail.com>
2022-08-30 21:51:54 -0400

    warmup2

--- August_2022/warmup_section.py
+++ August_2022/warmup_section.py
@@ -13,9 +13,20 @@ def lesser_of_two_evens(a, b):
             result = b 
     return result 
 
+def better_soluton(a, b):
+    if a%2 == 0 and b%2 == 0:
+        return min(a, b)
+    else:
+        return max(a, b)
+     
 def main():
     print(lesser_of_two_evens(2, 4))
     print(lesser_of_two_evens(2, 5))
+    
+    #better solution
+    print("\nEfficient Solution: ")
+    print(better_soluton(2, 4))
+    print(better_soluton(2, 5))
 
 if __name__ == "__main__":
     main()
