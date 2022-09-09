9c4ec13c5c18706a6e84ba31f83287ed4880dbc7
uthapa82 <tupendra022@gmail.com>
2022-09-08 22:00:26 -0400

    final

--- September_2022/paper_doll.py
+++ September_2022/paper_doll.py
@@ -28,7 +28,30 @@ def blackjack(a, b, c):
         return sum([a,b,c]) - 10
     else:
         return 'BUST'
+
+
+# summer of 69: return the sum of the numbers in the array, except ignore
+# sections of numbers starting with a 6 and extending to the next 9 
+# summer_69([1, 3, 5]) ---> 9
+# summer_69([4, 5, 6, 7, 8, 9]) ----->9
+def summer_69(arr):
+    total = 0
+    add = True
     
+    for num in arr:
+        while add:
+            if num!= 6:
+                total += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return total
 def main():
     list_1 = [1, 3, 3]
     print(has_33(list_1))
@@ -36,6 +59,9 @@ def main():
     print(blackjack(5, 6, 7))
     print(blackjack(9, 9, 9))
     print(blackjack(9, 9, 11))
+    print("Summer of 69")
+    print(summer_69([1, 3, 5]))
+    print(summer_69([4, 5, 6, 7, 8, 9]))
 
 if __name__ == "__main__":
     main()
