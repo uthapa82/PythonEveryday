d3339311a673e98bd23f3c90e258f4826076993f
uthapa82 <tupendra022@gmail.com>
2022-12-29 21:37:22 -0500

    d2 collections

--- hr_problems/dec_d2.py
+++ hr_problems/dec_d2.py
@@ -1,4 +1,5 @@
 from itertools import product
+from collections import Counter
 
 def cartesian_product():
     first_a = list(map(int, input("Enter A (x, y) (space separated): ").split()))
@@ -8,8 +9,22 @@ def cartesian_product():
     
     print(*result)
 
+def calculate_revenue():
+    no_of_shoes = int(input("Enter the total number of Shoes: "))
+    shoe_size = Counter(input("Enter sizes available (space separated): ").split())
+    number_of_customers = int(input("Enter total number of customers: "))
+    
+    total_amount = 0 
+    for shoe in range(number_of_customers):
+        size, amount = input("Enter Size and Cost of Shoe: ").split()
+        if size in shoe_size.elements():
+            total_amount += int(amount)
+            shoe_size.update({size: -1})
+    print("Total Revenue : $", total_amount)
+    
 def main():
     cartesian_product()
+    calculate_revenue()
     
 if __name__ == '__main__':
     main()
