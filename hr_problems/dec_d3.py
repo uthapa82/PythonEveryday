6e323825fa38a98c2860595fe57001c00157b0b3
uthapa82 <tupendra022@gmail.com>
2022-12-30 22:01:22 -0500

    day3 p2

--- hr_problems/dec_d3.py
+++ hr_problems/dec_d3.py
@@ -1,4 +1,6 @@
 # permutations are printed in a lexicographic sorted order 
+# A set is an unordered collection of elements without duplicate entries 
+
 from itertools import permutations
 
 def main():
@@ -8,5 +10,16 @@ def main():
     for item in sorted(result):
         print("".join(item))
 
+def average(array):
+    distinct_heights = set(array)
+    new_total_number = len(distinct_heights)
+    final_result = sum(distinct_heights) / new_total_number
+    
+    return final_result
+
 if __name__ == '__main__':
     main()
+    n = int(input("Enter the array size: "))
+    arr = list(map(int, input("Enter elements in array(space separated) : ").split()))
+    result = average(arr)
+    print(result)
