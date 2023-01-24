e0531c43fadc2e565fc94045800c801e61385b50
uthapa82 <tupendra022@gmail.com>
2023-01-23 21:44:17 -0500

    day 12

---
+++ hr_problems/jan_d12.py
@@ -0,0 +1,10 @@
+def main():
+    a = int(input('Enter number a: '))
+    b = int(input('Enter number b: '))
+    m = int(input('Enter number m: '))
+    
+    print(pow(a, b))
+    print(pow(a, b, m))
+    
+if __name__ == '__main__':
+    main()
