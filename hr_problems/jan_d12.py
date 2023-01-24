f81ed74c5a5d63e5f8c31ac58357aa8bb4386d73
uthapa82 <tupendra022@gmail.com>
2023-01-23 21:51:52 -0500

    day 12 final

--- hr_problems/jan_d12.py
+++ hr_problems/jan_d12.py
@@ -1,3 +1,12 @@
+def set_difference():
+    no_of_students_eng = int(input())
+    my_set_eng = set(map(int, input().split(' ')[:no_of_students_eng]))
+    
+    no_of_students_fren = int(input())
+    my_set_fren = set(map(int, input().split(' ')[:no_of_students_fren]))
+    
+    print(len(my_set_eng.difference(my_set_fren)))
+
 def main():
     a = int(input('Enter number a: '))
     b = int(input('Enter number b: '))
@@ -7,4 +16,4 @@ def main():
     print(pow(a, b, m))
     
 if __name__ == '__main__':
-    main()
+    set_difference()
