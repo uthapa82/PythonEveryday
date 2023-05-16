c5aba38fa96648c23c369f6d56133ea756cdf775
uthapa82 <tupendra022@gmail.com>
2023-05-15 21:57:10 -0400

    m day2 final

--- hr_problems/may2023/m15.py
+++ hr_problems/may2023/m15.py
@@ -3,6 +3,11 @@
     there's no limit in size as there is 2^31 - 1(c++ int) or 2^63 -1 (c++ long long int)
 
 """
+def traingle():
+    number = int(input())
+
+    for i in range(1, number):
+        print(10**i//9*i)
 
 def main():
     a = int(input())
@@ -13,6 +18,7 @@ def main():
     result = pow(a, b) + pow(c, d)
 
     print(result)
+    traingle()
 
 if __name__ == "__main__":
     main()
