cb92e42ee3e527213b4ff0de447a511b8a0917d2
uthapa82 <tupendra022@gmail.com>
2023-06-13 21:57:44 -0400

    june day 2 final

--- hr_problems/June2023/jun13.py
+++ hr_problems/June2023/jun13.py
@@ -3,7 +3,7 @@
 def mutation_operations(elements_in_A, number_of_operations):
     for i in range(0, number_of_operations):
         operation_name = input().split()
-        another_set = set(map(int, input().split(' ')))
+        another_set = set(map(int, input().split()))
 
         if operation_name[0] == 'intersection_update':
             elements_in_A.intersection_update(another_set)
@@ -11,17 +11,17 @@ def mutation_operations(elements_in_A, number_of_operations):
         elif operation_name[0] == 'update':
             elements_in_A.update(another_set)
 
-        elif operation_name[0] == 'symmetric_difference':
-            elements_in_A.symmetric_difference(another_set)
+        elif operation_name[0] == 'symmetric_difference_update':
+            elements_in_A.symmetric_difference_update(another_set)
         
-        else:
+        elif operation_name[0] == 'difference_update':
             elements_in_A.difference_update(another_set)
     
     print(sum(elements_in_A))
 
 def main():
     number_in_A = int(input())
-    elements_in_A = set(map(int, input().split(' ')[:number_in_A]))
+    elements_in_A = set(map(int, input().split()[:number_in_A]))
 
     number_of_operations = int(input())
     mutation_operations(elements_in_A, number_of_operations)
