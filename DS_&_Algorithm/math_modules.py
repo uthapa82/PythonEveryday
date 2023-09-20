969f7de75ac79203bb29b9695184302677d31814
uthapa82 <tupendra022@gmail.com>
2023-09-20 08:18:54 -0400

    python 9 Sept 23

--- DS_&_Algorithm/math_modules.py
+++ DS_&_Algorithm/math_modules.py
@@ -39,3 +39,58 @@ def gcd_optimal(A, B):
         return A
     
     return gcd(B, A % B)
+
+# time complexity theta(axb - max(a, b))
+def lcm(a, b):
+    res = max(a, b)
+
+    while True:
+
+        if res % a == 0 and res % b == 0:
+            return res 
+        
+        res += 1
+    
+    return res
+
+def optimal_lcm(a, b):
+
+    return a * b // gcd_optimal(a, b)
+
+# Time --> O(sqrt(n))
+def is_prime(n):
+    if n == 1:
+        return False 
+    
+    i = 2 
+    while (i * i <= n):
+
+        if n % i == 0:
+            return False 
+        
+        i += 1
+
+    return True 
+
+# 3 x is_prime(n)
+def is_prime_optimization(n):
+    if n == 1:
+        return False 
+    
+    if n == 2 or n == 3:
+        return True
+    
+    if  n == 2 or n == 3:
+        return True 
+    
+    if n % 2 == 0 or n % 3 == 0:
+        return False 
+    
+    i = 5
+    while (i * i <= n):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False 
+        
+        i += 6
+    
+    return True 
