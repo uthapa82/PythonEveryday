cd8ed15d299768defebf53b75dd45a30e0115371
uthapa82 <tupendra022@gmail.com>
2024-01-20 11:00:06 -0500

    correction

--- DS_&_Algorithm/math_modules.py
+++ DS_&_Algorithm/math_modules.py
@@ -80,9 +80,6 @@ def is_prime_optimization(n):
     if n == 2 or n == 3:
         return True
     
-    if  n == 2 or n == 3:
-        return True 
-    
     if n % 2 == 0 or n % 3 == 0:
         return False 
     
