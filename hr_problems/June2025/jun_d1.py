b55057e1e9d480c38de9890ad717bc8d848087b8
uthapa82 <tupendra022@gmail.com>
2025-06-08 16:27:30 -0400

    HR problems june-d1

---
+++ hr_problems/June2025/jun_d1.py
@@ -0,0 +1,41 @@
+import re
+
+#polynomial solve 
+def pol_solve():
+    x, k = map(int, input().split())
+    polynomial = input()
+    result = eval(polynomial.replace('x', str(x)))
+    print(result == k)
+
+#group(), groups() and groupdict()
+'''
+    Input: "abccde"
+
+    Regex matches 'cc', so m.group(1) returns 'c'.
+
+    m.group(1) if m else -1
+    If a match is found, return the first repeated character.
+
+    Otherwise, return -1.
+    
+    def first_repeating_char(s):
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            return s[i]
+    return -1
+
+    if __name__ == "__main__":
+        user_input = input().strip()
+        print(first_repeating_char(user_input))
+'''
+def group_problems():
+    import re 
+    m =re.search(r'([a-zA-z0-9])\1+', input().strip())
+    print(m.group(1) if m else -1)
+
+def main():
+    group_problems()
+
+
+if __name__ == '__main__':
+    main()
